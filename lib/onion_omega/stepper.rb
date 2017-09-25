2c5d014aff4976fe9584ee85938280bfa3448e06
Steve Occhipinti <dev@stevenocchipinti.com>
2017-09-25 13:00:29 +1000

    Implement half stepping
    This would have been much harder without the test!
    Testing this class is not easy because of the collaborators. Instead of mocking
    them out or testing a private method (like I am here), I think this step
    sequencing logic should probably by pulled out into its own simple class that
    can be easily tested.

--- lib/onion_omega/stepper.rb
+++ lib/onion_omega/stepper.rb
@@ -4,11 +4,13 @@ class OnionOmega::Stepper
   def initialize(
     range:,
     pins: [0, 1, 2, 3],
+    half_stepping: false,
     persist_to_file: nil,
     gpio: OnionOmega::GPIO.new
   )
     @range = range
     @pins = pins
+    @half_stepping = half_stepping
     @file = persist_to_file
     @current_step = (@file && File.exists?(@file)) ? File.read(@file).to_i : 0
     @gpio = gpio
@@ -46,12 +48,29 @@ class OnionOmega::Stepper
   end
 
   def step(increment = 1)
-    new_pins = @pins.each_with_index.map do |pin, index|
-      index == (@current_step % @pins.size) ? 1 : 0
+    set_pins(*@half_stepping ? half_step : full_step).tap do
+      @current_step += increment
+      File.write @file, @current_step if @file
     end
-    set_pins(*new_pins)
-    @current_step += increment
-    File.write @file, @current_step if @file
+  end
+
+  def full_step
+    @pins.each_with_index.map do |pin, index|
+      phase = @current_step % @pins.size
+      index == phase ? 1 : 0
+    end
+  end
+
+  def half_step
+    phase = @current_step % (@pins.size * 2)
+    @pins.each_with_index.map do |pin, index|
+      case index * 2
+      when phase, phase - 1, (phase + 1) % (@pins.size * 2)
+        1
+      else
+        0
+      end
+    end
   end
 
   def set_pins(*pins)
--- test/onion_omega_test.rb
+++ test/onion_omega_test.rb
@@ -1,11 +1,44 @@
 require 'test_helper'
+require_relative "../lib/onion_omega/stepper"
 
 class OnionOmegaTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::OnionOmega::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_full_stepping_works
+    gpio = OnionOmega::GPIO.new(dry_mode: true)
+    stepper = OnionOmega::Stepper.new(
+      range: 100,
+      gpio: gpio,
+      half_stepping: false
+    )
+    assert_equal stepper.send(:step), [1,0,0,0] # 0
+    assert_equal stepper.send(:step), [0,1,0,0] # 1
+    assert_equal stepper.send(:step), [0,0,1,0] # 2
+    assert_equal stepper.send(:step), [0,0,0,1] # 3
+
+    assert_equal stepper.send(:step), [1,0,0,0] # 0
+    assert_equal stepper.send(:step), [0,1,0,0] # 1
+    assert_equal stepper.send(:step), [0,0,1,0] # 2
+    assert_equal stepper.send(:step), [0,0,0,1] # 3
+  end
+
+  def test_half_stepping_works
+    gpio = OnionOmega::GPIO.new(dry_mode: true)
+    stepper = OnionOmega::Stepper.new(
+      range: 100,
+      gpio: gpio,
+      half_stepping: true
+    )
+
+    assert_equal stepper.send(:step), [1,0,0,0] # 0
+    assert_equal stepper.send(:step), [1,1,0,0] # 1
+    assert_equal stepper.send(:step), [0,1,0,0] # 2
+    assert_equal stepper.send(:step), [0,1,1,0] # 3
+    assert_equal stepper.send(:step), [0,0,1,0] # 4
+    assert_equal stepper.send(:step), [0,0,1,1] # 5
+    assert_equal stepper.send(:step), [0,0,0,1] # 6
+    assert_equal stepper.send(:step), [1,0,0,1] # 7
   end
 end
